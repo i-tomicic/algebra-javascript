6efd92a215bb1de454f1969282a42f7057ff8403
i-tomicic <itomicic@gmail.com>
2023-01-26 21:28:37 +0100

    start of hands-on 5

---
+++ js-examples/script.js
@@ -0,0 +1,89 @@
+/*
+
+// Inicijalizacija varijabli?
+
+var firstName = "Tony";
+var firstName = "John";
+var lastName = "Doe";
+var price = 5;
+var discount = 78;
+var fullPrice = 0;
+var _break = [];
+var $myObject = {};
+
+console.log(price);
+console.log(firstName);
+console.log(lastName);
+
+// Kraj mog programa
+
+var tekst = `algebra ${price + 1}`;
+console.log(tekst);
+
+console.log(typeof NaN);
+
+*/
+
+/* Pratite kod u script.js
+1. Nađite varijable u listi koje su iste po vrijednosti.
+2. Koja je vrijednost varijable x na kraju programa? Koji je to tip podatka?
+3. Koja je vrijednost varijable baaa? Objasnite!
+4. Provjerite tip podatka varijable w. */
+
+// var x = "Lorem ipsum",
+//   y = 2345,
+//   z = "2345",
+//   q = false,
+//   w;
+
+// x += "3" + 1;
+// var baaa = "b" + "a" + +"a" + "a";
+
+// console.log(x);
+// console.log(baaa);
+// console.log(typeof w);
+// console.log(y == z);
+
+// var text = " Lorem ipsum dolor sit amet";
+// console.log(text.length);
+// text = text.replace("amet", "elit");
+// console.log(text);
+// text += ", consectetur adipiscing elit";
+// console.log(text);
+// text = text.toUpperCase();
+// console.log(text);
+// text = text.trim();
+// console.log(text);
+// console.log(text.charAt(12));
+// console.log(text.indexOf("S"));
+// console.log(text.substring(4, 18));
+
+// Hands-on First Task
+
+// const printLines = function () {
+//   console.log(secondLine);
+
+//   let firstLine = "I'm your first line!";
+//   console.log(firstLine);
+
+//   let secondLine = "I'm your second line";
+//   console.log(secondLine);
+// };
+
+// printLines();
+
+// Hands-on Second Task
+
+/* const madeOfGlass = function () {
+  const thing = "window";
+  thing = "glass";
+
+  console.log(thing);
+};
+
+madeOfGlass(); */
+
+// Hands-on Third Task
+
+let first = [1, 2, 3];
+let second = [2, 4, 6];
